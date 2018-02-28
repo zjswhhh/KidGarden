d52502d8035fc04c63d7a293cfbbfc611f4a6567
Jing Zhang <zjsw1206@gmail.com>
2018-02-28 16:29:21 -0500

    Update load.sql
    change dat paths

--- SQL&data/load.sql
+++ SQL&data/load.sql
@@ -1,44 +1,44 @@
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/type.dat"
+LOAD DATA LOCAL INFILE "type.dat"
 INTO TABLE Type
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/user.dat"
+LOAD DATA LOCAL INFILE "user.dat"
 INTO TABLE Userdb
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/parents.dat"
+LOAD DATA LOCAL INFILE "parents.dat"
 INTO TABLE Parents
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/class.dat"
+LOAD DATA LOCAL INFILE "class.dat"
 INTO TABLE Class
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/teacher.dat"
+LOAD DATA LOCAL INFILE "teacher.dat"
 INTO TABLE Teacher
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/nutritionist.dat"
+LOAD DATA LOCAL INFILE "nutritionist.dat"
 INTO TABLE Nutritionist
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/kid_basicinfo.dat"
+LOAD DATA LOCAL INFILE "kid_basicinfo.dat"
 INTO TABLE Kid_basicINFO
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/kid_physical.dat"
+LOAD DATA LOCAL INFILE "kid_physical.dat"
 INTO TABLE Kid_Physical
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/meal_nutrit.dat"
+LOAD DATA LOCAL INFILE "meal_nutrit.dat"
 INTO TABLE Meal_Nutrit
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/disease.dat"
+LOAD DATA LOCAL INFILE "disease.dat"
 INTO TABLE Disease
 FIELDS TERMINATED BY "<>";
 
-LOAD DATA LOCAL INFILE "/home/flan/milestone3/TaskC/medical_history.dat"
+LOAD DATA LOCAL INFILE "medical_history.dat"
 INTO TABLE Medical_History
 FIELDS TERMINATED BY "<>";
 
